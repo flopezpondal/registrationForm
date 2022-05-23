0bbf6df14d02ccd0738f2af903befd9612fa7a23
flopezpondal <federicolopezpndl@gmail.com>
2022-05-23 17:24:26 -0300

    form actualizado

--- scripts/form-user-register.js
+++ scripts/form-user-register.js
@@ -1,6 +1,29 @@
 // Tomar el elemento formulario
 
 const userRegisterForm = document.getElementById('user-register');
+
+const loginSection = document.getElementById('loginSection');
+const logoutSection = document.getElementById('logout');
+let user;
+
+checkIsAuth();
+
+function checkIsAuth(){    
+
+    user = JSON.parse(localStorage.getItem('currentUser'));
+    
+    if(user){
+        loginSection.classList.add('d-none');
+        logoutSection.classList.remove('d-none');
+        const welcomeHTML = document.querySelector('.welcome');
+        welcomeHTML.textContent = `Bienvenido ${user.fullname}`;
+    } else{
+        loginSection.classList.remove('d-none');
+        logoutSection.classList.add('d-none');
+    }
+
+}
+
 const formEl = userRegisterForm.elements;
 const errorMSG = document.getElementById('form-error');
 const usersList = document.getElementById('users-list');
@@ -24,13 +47,13 @@ function registrarUsuario(ev) {
     const emailExist = users.some(usr => email === usr.email);
 
     if (emailExist) {
-        showErrorMsg('El correo ya existe');
+        showErrorMsg('El correo ya existe', errorMSG);
         return;
     }
 
-    if(elemento.password.value != elemento.password2.value) return showErrorMsg('Las constraseñas no coinciden');
+    if(elemento.password.value != elemento.password2.value) return showErrorMsg('Las constraseñas no coinciden', errorMSG);
 
-    if (elemento.age.valueAsNumber < 18) return showErrorMsg('Debe ser mayor de 18 años para registrarse');
+    if (elemento.age.valueAsNumber < 18) return showErrorMsg('Debe ser mayor de 18 años para registrarse', errorMSG);
 
     console.log(ev);
     const user = {
@@ -51,10 +74,10 @@ function registrarUsuario(ev) {
     renderUser();
 }
 
-function showErrorMsg(message, time = 3000) {
-    errorMSG.innerText = message;
+function showErrorMsg(message, element, time = 3000) {
+    element.innerText = message;
     setTimeout(() => {
-        errorMSG.innerText = null;
+        element.innerText = null;
     }, time);
 }
 
@@ -62,6 +85,48 @@ function renderUser() {
     const users = JSON.parse(localStorage.getItem('users')) || [];
     usersList.innerHTML = '';
     users.forEach(user => {
-        usersList.innerHTML += `<li>${user.fullname}</li>`
+        usersList.innerHTML += `<li><img class="avatar" src="${user.avatar || ''}"></div>
+        <div class="data">
+            <div class="name">${user.fullname}</div>
+            <div class="user">${user.username}</div>
+            <div class="email">${user.email}</div>
+        </div>
+        <div class="age">${user.age}</div>
+        <div class="role">${user.role}</div>
+        </li>`
     });
+}
+
+// Obtener referencia al formulario de login
+const loginForm = document.getElementById('login');
+const formLoginError = document.getElementById('formLoginError');
+//Detectar cuando se dispara el evento submit
+// Obtener valores de email y password en el input
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputEmail = loginForm.elements.email.value;
+    const inputPassword = loginForm.elements.password.value;
+    const user = users.find(usr => {
+        return usr.email === inputEmail;
+    });
+    console.log('user', user);
+    
+    if(!user || user.password != inputPassword){
+        showErrorMsg('Credenciales incorrectas', formLoginError);
+        return;
+    }
+
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    loginForm.requestFullscreen();
+    checkIsAuth();
+    // window.location.href = '/index.html';
+    setTimeout(() => window.location = '/pages/login/login.html', 3000);
+    window.location.href = '/login.html';
+})
+
+function logout() {
+    localStorage.removeItem('currentUser');
+    checkIsAuth();
+    setTimeout(() => window.location = '/pages/login/login.html', 3000);
+    window.location.href = '/login.html';    
 }
